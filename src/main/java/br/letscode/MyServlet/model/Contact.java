225316e52df41beec7a0e8fcfe146272d734d412
Lucio <lucio.oliveira@zup.com.br>
2022-02-14 21:15:12 -0300

    servlet

---
+++ src/main/java/br/letscode/MyServlet/model/Contact.java
@@ -0,0 +1,29 @@
+package br.letscode.MyServlet.model;
+
+public class Contact {
+    private String name;
+    private String email;
+
+    public Contact(){}
+
+    public Contact(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
